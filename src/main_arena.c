b579598211fb984a3722ce69b5f5f45e797f4f00
BensonLing0925 <benson920925@gmail.com>
2025-12-30 16:45:48 +0800

    finish token layer

--- src/main_arena.c
+++ src/main_arena.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <limits.h>
 #include <unistd.h>
+#include <ctype.h>
 #include <sys/wait.h>
 
 #include "arena.h"
@@ -13,12 +14,65 @@
 
 #define NUM_COMMAND 5
 
+#define DEFAULT_NUM_TOKENS 10
 #define DEFAULT_NUM_ARG 8
+#define DEFAULT_REDIR_CAP 6
+
+enum RedirType {
+    R_IN,
+    R_OUT,
+    R_OUT_APPEND,
+    R_ERR,
+    R_ERR_APPEND,
+    R_NONE
+};
+
+struct Redir {
+    int fd;
+    enum RedirType rd_type;
+    const char* path;
+};
+
+enum TokenType {
+    TOK_WORD,
+    TOK_REDIR,
+    TOK_PIPE,
+    TOK_NONE
+};
+
+struct Token {
+    enum TokenType tok_type;
+    enum RedirType rd_type;
+    int fd;
+    char* text;
+};
+
+struct TokenList {
+    struct Token* tokens;
+    size_t ntoks;
+    size_t tok_cap;
+};
+
+void token_init(struct Token* token) {
+    token->tok_type = TOK_NONE;
+    token->rd_type = R_NONE;
+    token->fd = -1;
+    token->text = NULL;
+}
+
+void toklist_init(struct TokenList* toklist) {
+    toklist->tokens = NULL;
+    toklist->ntoks = 0;
+    toklist->tok_cap = 0;
+}
 
 struct Cmd {
   size_t argc;
   char** argv;
   size_t cap;
+  struct Redir* rds;
+  size_t nrds;
+  size_t rd_cap;
 };
 
 struct Cmd* createCmd();
@@ -30,18 +84,43 @@ void initCmd(struct Cmd* cmd) {
   cmd->argc = 0;
   cmd->argv = NULL;
   cmd->cap = DEFAULT_NUM_ARG;
+  cmd->rds = NULL;
+  cmd->nrds = 0;
+  cmd->rd_cap = DEFAULT_REDIR_CAP;
+}
+
+void initCmdRedir(struct Cmd* cmd, struct arena* a) {
+    cmd->rds = arena_alloc(a, cmd->rd_cap * sizeof(struct Redir));
+    if (cmd->rds == NULL) {
+        perror("Not enough memory at initCmdRedir");
+        return;
+    }
 }
 
 void initCmdArgv(struct Cmd* cmd, struct arena* a) {
-    cmd->argv = arena_alloc(a, DEFAULT_NUM_ARG * sizeof(char*));
+    cmd->argv = arena_alloc(a, cmd->cap * sizeof(char*));
     if (cmd->argv == NULL) {
         perror("Not enough memory at initCmdArgv");
         return;
     }
-
     for ( size_t i = 0 ; i < cmd->cap ; ++i ) {
         cmd->argv[i] = NULL;
     }
+}
+
+int cmdRedirGrow(struct Cmd* cmd, struct arena* a) {
+    size_t old = cmd->rd_cap;
+    size_t new = old * 2;
+
+    struct Redir* v = arena_alloc(a, sizeof(struct Redir) * new);
+    if (!v) return -1;
+
+    memcpy(v, cmd->rds, sizeof(struct Redir) * old);
+    memset(v + old, 0, sizeof(struct Redir) * (new - old));
+
+    cmd->rds = v;
+    cmd->rd_cap  = new;
+    return 0;
 }
 
 int cmdArgvGrow(struct Cmd* cmd, struct arena* a) {
@@ -56,6 +135,35 @@ int cmdArgvGrow(struct Cmd* cmd, struct arena* a) {
 
     cmd->argv = v;
     cmd->cap  = new;
+    return 0;
+}
+
+int tokListGrow(struct TokenList* toklist, struct arena* a) {
+    size_t old = toklist->tok_cap;
+    size_t new = old * 2;
+    if (old == 0) new = DEFAULT_NUM_TOKENS;
+
+    struct Token* v = arena_alloc(a, sizeof(struct Token) * new);
+    if (!v) return -1;
+
+    memcpy(v, toklist->tokens, sizeof(struct Token) * old);
+    memset(v + old, 0, sizeof(struct Token) * (new - old));
+
+    toklist->tokens = v;
+    toklist->tok_cap  = new;
+
+    for (size_t num_tok = old ; num_tok < toklist->tok_cap ; num_tok++) {
+        token_init(&toklist->tokens[num_tok]);
+    }
+
+    return 0;
+}
+
+int toklist_push(struct TokenList* toklist, struct arena* a, struct Token token) {
+    if (toklist->ntoks == toklist->tok_cap) {
+        if (tokListGrow(toklist, a) < 0) return -1;
+    }
+    toklist->tokens[toklist->ntoks++] = token;
     return 0;
 }
 
@@ -81,15 +189,39 @@ int isDelimiter(char ch) {
     return 0;
 }
 
-static int push_token(struct Cmd* cmd, struct arena* a,
+static void setToken(struct Token* token, const char* token_str) {
+    if ((!strcmp(token_str, "1>")) || (!strcmp(token_str, ">"))) {
+        token->tok_type = TOK_REDIR;
+        token->fd = 1;
+        token->rd_type = R_OUT;
+        return;
+    }
+    if ((!strcmp(token_str, ">>")) || (!strcmp(token_str, "1>>"))) {
+        token->tok_type = TOK_REDIR;
+        token->fd = 1;
+        token->rd_type = R_OUT_APPEND;
+        return;
+    }
+    if ((!strcmp(token_str, "2>"))) {
+        token->tok_type = TOK_REDIR;
+        token->fd = 2;
+        token->rd_type = R_ERR;
+        return;
+    }
+    if ((!strcmp(token_str, "2>>"))) {
+        token->tok_type = TOK_REDIR;
+        token->fd = 2;
+        token->rd_type = R_ERR_APPEND;
+        return;
+    }
+    token->tok_type = TOK_WORD;
+    token->fd = -1;
+    token->rd_type = R_NONE;
+}
+
+static int push_token(struct TokenList* toklist, struct arena* a,
                       const char* token, int token_len) {
     if (token_len <= 0) return 0;
-
-    // TODO: grow argv (選 A 或 B)
-    if (cmd->argc + 1 >= cmd->cap) {
-        // 如果 cmd->argv 也改 arena：cmdArgvGrow_arena(cmd,a)
-        if (cmdArgvGrow(cmd, a) < 0) return -1;
-    }
 
     char* mem = arena_alloc(a, (size_t)token_len + 1);
     if (!mem) return -1;
@@ -97,16 +229,19 @@ static int push_token(struct Cmd* cmd, struct arena* a,
     memcpy(mem, token, (size_t)token_len);
     mem[token_len] = '\0';
 
-    cmd->argv[cmd->argc++] = mem;
-    cmd->argv[cmd->argc] = NULL;
+    struct Token struct_token;
+    struct_token.text = mem;
+    setToken(&struct_token, mem);
+    toklist_push(toklist, a, struct_token);
+    
     return 0;
 }
 
-static int emit_token(struct Cmd* cmd, struct arena* a, 
+static int emit_token(struct TokenList* toklist, struct arena* a, 
                   char* token, int* token_index) {
 
     if (*token_index == 0) return 0;
-    if (push_token(cmd, a, token, *token_index) < 0) // push_token should add '\0'
+    if (push_token(toklist, a, token, *token_index) < 0) // push_token should add '\0'
         return -1;
     *token_index = 0;
     token[0] = '\0';
@@ -114,17 +249,18 @@ static int emit_token(struct Cmd* cmd, struct arena* a,
 
 }
 
-ssize_t tokenize(char *str, struct Cmd *cmd, struct arena *a) {
+ssize_t tokenize(struct TokenList* toklist, char* str, struct arena *a) {
     int i = 0;
     char token[DEFAULT_STR_ALLOC];
     int n = 0;
+
 
     token[0] = '\0';
     while (str[i] != '\0') {
         char c = str[i];
         // 1) whitespace: end token
         if (c == ' ') {
-            if (emit_token(cmd, a, token, &n) < 0) return -1;
+            if (emit_token(toklist, a, token, &n) < 0) return -1;
             // skip all spaces
             do { i++; } while (str[i] == ' ');
             continue;
@@ -173,13 +309,74 @@ ssize_t tokenize(char *str, struct Cmd *cmd, struct arena *a) {
         if (c == '\\') {
             i++;
         }
-        // 5) normal char
+        // 5) redirect
+        if (isalpha(c)) {
+            if (str[i+1] == '>') {
+                token[n++] = c;
+                i++;
+                token[n++] = str[i++];
+                if (str[i] == '>') {
+                    token[n++] = c;
+                    token[n++] = str[i++];
+                }
+                if (emit_token(toklist, a, token, &n) < 0) return -1;
+            }
+        }
+        // 6) normal char
         if (n + 1 >= DEFAULT_STR_ALLOC) { errno = EOVERFLOW; return -1; }
         token[n++] = str[i++];
     }
     // end of input: emit last token
-    if (emit_token(cmd, a, token, &n) < 0) return -1;
+    if (emit_token(toklist, a, token, &n) < 0) return -1;
     return 0;
+}
+
+static void printTokenType(enum TokenType tok_type) {
+    if (tok_type == TOK_WORD) printf("WORD");
+    if (tok_type == TOK_REDIR) printf("REDIR");
+    if (tok_type == TOK_PIPE) printf("PIPE");
+    if (tok_type == TOK_NONE) printf("NONE");
+}
+
+static void printRedirType(enum RedirType rd_type) {
+    if (rd_type == R_IN) printf("R_IN");
+    if (rd_type == R_OUT) printf("R_OUT");
+    if (rd_type == R_OUT_APPEND) printf("R_OUT_APPEND");
+    if (rd_type == R_ERR) printf("R_ERR");
+    if (rd_type == R_ERR_APPEND) printf("R_ERR_APPEND");
+    if (rd_type == R_NONE) printf("R_NONE");
+}
+
+void print_toklist(struct TokenList* toklist) {
+    size_t num_tokens = toklist->ntoks;
+
+    printf("number of tokens: %zu\n", toklist->ntoks);
+    printf("toklist token caps: %zu\n", toklist->tok_cap);
+
+    for ( size_t i = 0 ; i < num_tokens ; ++i ) {
+        struct Token token = toklist->tokens[i];
+        printTokenType(token.tok_type);
+        printf("{ (%s), fd: %d, ", token.text, token.fd);
+        printRedirType(token.rd_type);
+        printf("}\n");
+    }
+
+}
+
+int push_argv(struct Cmd* cmd, struct arena* a, char* text) {
+    if (cmd->argc + 1 >= cmd->cap ) {
+        if (cmdArgvGrow(cmd, a) < 0) return -1;
+    }
+    size_t n = strlen(text);
+    cmd->argv[cmd->argc++] = text;
+    cmd->argv[cmd->argc] = NULL;
+    return 0;
+}
+
+static int parse_toklist(struct Cmd* cmd, struct TokenList* toklist, struct arena* a) {
+    for ( size_t i = 0 ; i < toklist->ntoks ; ++i ) {
+        if (toklist->tokens[i])
+    }
 }
 
 static void chomp_newline(char *s) {
@@ -383,8 +580,26 @@ int main() {
     struct Cmd cmd;
     initCmd(&cmd);
     initCmdArgv(&cmd, &a);
-    // tokenize(cmd_str, " ", cmd);
-    tokenize(cmd_str, &cmd, &a);
+    struct TokenList toklist;
+    toklist_init(&toklist);
+    tokenize(&toklist, cmd_str, &a);
+
+#ifdef TOKENIZER_DEBUG
+    print_toklist(&toklist);
+    free(cmd_str);
+    arena_destroy(&a);
+    return 0;
+#endif
+
+    parse_toklist(&cmd, &toklist, &a);
+
+    /*TODO:
+    1. check toklist implementation works or not
+    2. add tokenizer to handle redirect operator
+    3. add parser layer to interpret toklist(Token layer)
+    4. hook up parser result to cmd
+    */
+
     char* exe_name = cmd.argv[0];
 
     /* get PATH */
